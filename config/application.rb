550ff88005faa608493717dc8d629be456fe8e77
jkirky82 <jkirky82@gmail.com>
2021-08-12 17:04:49 +1000

    debugging heroku stripe

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Marketplace
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     # Configuration for the application, engines, and railties goes here.
-    config.serve_static_assest =true
+    config.serve_static_assest = true
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
