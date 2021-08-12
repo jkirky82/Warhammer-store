c85bae7f5d2eef148d3d3c7497e18b7f7057d020
jkirky82 <jkirky82@gmail.com>
2021-08-12 15:37:00 +1000

    Added santization

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,4 +9,15 @@ class Listing < ApplicationRecord
   validates :title, presence: true   
   validates :description, presence: true   
   validates :price, presence: true   
+
+  #used for data santization for listing 
+  before_save :remove_whitespace
+
+  private
+
+  #removes all white spaces from title and description input
+  def remove_whitespace
+    self.title = self.title.strip
+    self.description = self.description.strip
+  end
 end
